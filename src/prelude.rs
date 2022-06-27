3b1f7262d77c431a3544a21b38693b5216e77272
Tibor Schneider <sctibor@ethz.ch>
2022-06-27 16:43:32 +0200

    fixed oops

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+//! Convenience re-export of common members.
+
+pub use crate::bgp::BgpSessionType;
+pub use crate::interactive::InteractiveNetwork;
+pub use crate::network::Network;
+pub use crate::record::RecordNetwork;
+pub use crate::types::{AsId, NetworkError, Prefix, RouterId};
