dca6d275160182952d0fa03a159cbbfdeea2f3eb
Tibor Schneider <sctibor@ethz.ch>
2022-11-24 11:01:13 +0100

    Added darker color variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,31 +48,37 @@ module.exports = {
                 'red': {
                     DEFAULT: 'rgb(var(--color-red) / <alpha-value>)',
                     dark: 'rgb(var(--color-red-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-red-darker) / <alpha-value>)',
                 },
 
                 'orange': {
                     DEFAULT: 'rgb(var(--color-orange) / <alpha-value>)',
                     dark: 'rgb(var(--color-orange-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-orange-darker) / <alpha-value>)',
                 },
 
                 'yellow': {
                     DEFAULT: 'rgb(var(--color-yellow) / <alpha-value>)',
                     dark: 'rgb(var(--color-yellow-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-yellow-darker) / <alpha-value>)',
                 },
 
                 'blue': {
                     DEFAULT: 'rgb(var(--color-blue) / <alpha-value>)',
                     dark: 'rgb(var(--color-blue-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-blue-darker) / <alpha-value>)',
                 },
 
                 'green': {
                     DEFAULT: 'rgb(var(--color-green) / <alpha-value>)',
                     dark: 'rgb(var(--color-green-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-green-darker) / <alpha-value>)',
                 },
 
                 'purple': {
                     DEFAULT: 'rgb(var(--color-purple) / <alpha-value>)',
                     dark: 'rgb(var(--color-purple-dark) / <alpha-value>)',
+                    darker: 'rgb(var(--color-purple-darker) / <alpha-value>)',
                 },
             }
         },
