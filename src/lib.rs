28572877b8478c1c3530c9cd5c0e2320596926f5
Tibor Schneider <sctibor@ethz.ch>
2023-01-19 17:28:22 +0100

    updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+#![doc(html_logo_url = "https://iospf.tibors.ch/images/bgpsim/dark_only.svg")]
+
 use proc_macro::TokenStream;
 
 mod ip;
