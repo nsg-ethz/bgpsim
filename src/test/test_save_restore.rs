9a2fecdc9f9252a0d6daf88e1ae242eb77bb88e6
Tibor Schneider <sctibor@ethz.ch>
2024-02-05 18:15:15 +0100

    Add more LocalOspf tests

--- src/test/test_save_restore.rs
+++ src/test/test_save_restore.rs
@@ -27,12 +27,13 @@ mod t {
         },
         event::BasicEventQueue,
         network::Network,
+        ospf::{GlobalOspf, LocalOspf, OspfImpl},
         topology_zoo::TopologyZoo,
         types::{Ipv4Prefix, Prefix, SimplePrefix, SinglePrefix},
     };
 
-    fn get_net<P: Prefix>() -> Network<P, BasicEventQueue<P>> {
-        let mut net: Network<P, _> = TopologyZoo::Abilene.build(BasicEventQueue::new());
+    fn get_net<P: Prefix, Ospf: OspfImpl>() -> Network<P, BasicEventQueue<P>, Ospf> {
+        let mut net: Network<P, _, Ospf> = TopologyZoo::Abilene.build(BasicEventQueue::new());
         net.build_external_routers(extend_to_k_external_routers, 3)
             .unwrap();
         net.build_link_weights(uniform_integer_link_weight, (10, 100))
@@ -47,8 +48,8 @@ mod t {
     }
 
     #[test]
-    fn export<P: Prefix>() {
-        let net = get_net::<P>();
+    fn export<P: Prefix, Ospf: OspfImpl>() {
+        let net = get_net::<P, Ospf>();
         let json_str = net.as_json_str();
         // check that the two attributes are present
         let json_obj: Value = serde_json::from_str(&json_str).unwrap();
@@ -67,52 +68,61 @@ mod t {
     }
 
     #[test]
-    fn import_net<P: Prefix>() {
-        let net = get_net::<P>();
-        let restored: Network<P, _> =
+    fn import_net<P: Prefix, Ospf: OspfImpl>() {
+        let net = get_net::<P, Ospf>();
+        let restored: Network<P, _, Ospf> =
             Network::from_json_str(&net.as_json_str(), BasicEventQueue::default).unwrap();
         assert!(restored.weak_eq(&net));
     }
 
     #[test]
-    fn import_with_config<P: Prefix>() {
-        let net = get_net::<P>();
+    fn import_with_config<P: Prefix, Ospf: OspfImpl>() {
+        let net = get_net::<P, Ospf>();
         let json_str = net.as_json_str();
         let mut json_obj: Value = serde_json::from_str(&json_str).unwrap();
         let _ = json_obj["net"].take();
         let modified_json_str = serde_json::to_string(&json_obj).unwrap();
-        let restored: Network<P, _> =
+        let restored: Network<P, _, Ospf> =
             Network::from_json_str(&modified_json_str, BasicEventQueue::default).unwrap();
         assert!(restored.weak_eq(&net));
     }
 
     #[test]
-    fn import_wrong_net<P: Prefix>() {
-        let net = get_net::<P>();
+    fn import_wrong_net<P: Prefix, Ospf: OspfImpl>() {
+        let net = get_net::<P, Ospf>();
         let json_str = net.as_json_str();
         let mut json_obj: Value = serde_json::from_str(&json_str).unwrap();
         let _ = json_obj["net"]["routers"].take();
         let modified_json_str = serde_json::to_string(&json_obj).unwrap();
-        let restored: Network<P, _> =
+        let restored: Network<P, _, Ospf> =
             Network::from_json_str(&modified_json_str, BasicEventQueue::default).unwrap();
         assert!(restored.weak_eq(&net));
     }
 
     #[test]
-    fn import_compact_net<P: Prefix>() {
-        let net = get_net::<P>();
+    fn import_compact_net<P: Prefix, Ospf: OspfImpl>() {
+        let net = get_net::<P, Ospf>();
         let json_str = net.as_json_str_compact();
-        let restored: Network<P, _> =
+        let restored: Network<P, _, Ospf> =
             Network::from_json_str(&json_str, BasicEventQueue::default).unwrap();
         assert!(restored.weak_eq(&net));
     }
 
-    #[instantiate_tests(<SinglePrefix>)]
-    mod single {}
+    #[instantiate_tests(<SinglePrefix, GlobalOspf>)]
+    mod single_global {}
 
-    #[instantiate_tests(<SimplePrefix>)]
-    mod simple {}
+    #[instantiate_tests(<SimplePrefix, GlobalOspf>)]
+    mod simple_global {}
 
-    #[instantiate_tests(<Ipv4Prefix>)]
-    mod ipv4 {}
+    #[instantiate_tests(<Ipv4Prefix, GlobalOspf>)]
+    mod ipv4_global {}
+
+    #[instantiate_tests(<SinglePrefix, LocalOspf>)]
+    mod single_local {}
+
+    #[instantiate_tests(<SimplePrefix, LocalOspf>)]
+    mod simple_local {}
+
+    #[instantiate_tests(<Ipv4Prefix, LocalOspf>)]
+    mod ipv4_local {}
 }
